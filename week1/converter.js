d2fc74bda627374411e077351fab53529c3e271a
meem08 <akinsuyi.blessing08@gmail.com>
2026-09-19 08:07:19 +0100

    created my week1 files

---
+++ week1/converter.js
@@ -0,0 +1,16 @@
+const rate = 1300
+function nariaToUsd(amount) {
+   return amount/rate 
+}
+function usdToNaria(amount) {
+    return amount*rate
+}
+function celsiusToFahrenheit(celsius) {
+   return celsius *9/5 + 32
+}
+function kgToPounds(kg) {
+    return kg * 2.20462
+}
+console.log(celsiusToFahrenheit(0))
+console.log(celsiusToFahrenheit(100))
+console.log(usdToNaria(nariaToUsd(5000)))
