d1a546a90f4f62ffeab834d412a5cc9d08307b5e
bakinsuy <blessing.akinsuyi01@gmail.com>
2026-09-22 13:21:11 +0100

    guess.js completed

--- week1/day2/guess.js
+++ week1/day2/guess.js
@@ -3,20 +3,39 @@ const keepGuess = []
 alert("Hello guess the secret number between 1 and 100")
 let guess = 1;
 while (guess <= 7) {
-    let guessNum = Number(window.prompt("Enter number between 1 and 100"));
+    let input = window.prompt("Enter number between 1 and 100")
+    if (input === null) {
+        alert("you  quit!!!")
+        break;
+    }
+    let guessNum = Number(input);
     guess++
     keepGuess.push(guessNum)
 
-    if (guessNum == secretNumber) {
+    if (guessNum === secretNumber) {
+        alert(`Your guess number is Perfect!,${secretNumber}`)
         console.log(`Your guess number is Perfect!,${secretNumber}`);
         break;
     }
+    if (guess === 7) {
+        alert(`You have used up all your ${guess} attempts.`)
+        console.log(`You have used up all your ${guess} attempts.`)
+
+    }
     if (guessNum > secretNumber) {
+        alert("Your number is too high, try lower numbers")
         console.log("Your number is too high, try lower numbers")
     }
     if (guessNum < secretNumber) {
+        alert("Your number is too low, try higher numbers")
         console.log("Your number is too low, try higher numbers")
     }
+    if (isNaN(guessNum)) {
+        alert("This is not a number, Enter a number!!")
+        console.log("This is not a number, Enter a number!!")
+    }
+
+
 }
 console.log("Your guesses: ", keepGuess);
 
