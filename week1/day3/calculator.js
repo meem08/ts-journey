b65e1526cc6470e1eb5eb48c9d7c84bc11dcf790
meem08 <akinsuyi.blessing08@gmail.com>
2026-09-26 11:26:00 +0100

    calculator

--- week1/day3/calculator.js
+++ week1/day3/calculator.js
@@ -1,5 +1,7 @@
-function calculator(function(), a,b){
-   return function(a,b);
+// const operator = function (){}
+
+function calculator(operator, a, b) {
+   return operator(a,b);
 }
 
 function add(a, b){
@@ -13,8 +15,11 @@ function multiply(a, b){
 }
 function divide(a, b){
    if (b == 0){
-      return "It can be divided"
+      return "It cannot be divided"
    }
    return a/b;
 }
-console.log(calculator(divide(5,1)))
+console.log(calculator(add, 6, 4))
+console.log(calculator(subtract, 7, 1))
+console.log(calculator(multiply, 25 , 2))
+console.log(calculator(divide, 5 , 1))
